fd9960fe938ada1b3994c2c3f870662b7f2730db
gilarc <gilarc@tuta.io>
2024-01-27 09:28:33 +0700

    code rev

--- hello_world.rb
+++ hello_world.rb
@@ -2,8 +2,8 @@
 # Copyright (c) 2024 Gillar Ajie Prasatya
 # See LICENSE file for details.
 
-sections = ["web", "tools", "science", "systems",
-            "embedded", "drivers", "mobile"]
+sections = %W[web tools science systems 
+			embedded drivers mobile]
 sections.each do |section|
   puts "Hello, #{section} developers!"
 end
