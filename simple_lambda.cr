fa2a807b1522534e997314a7bc88e0d3c181baa4
gilarc <gilarc@tuta.io>
2024-01-30 18:40:38 +0700

    item rev

--- simple_lambda.cr
+++ simple_lambda.cr
@@ -7,6 +7,6 @@ puts -> (x: Int32, y: Int32) {x * y + 1}.call(6,2)
 # lambda versi crystal. kode diatas ditulis dalam bahasa Crystal menggunakan lambda,
 # dengan parameter `x` dan `y`, yang harus bertipe data `Int32`
 # ditulis `(x: Int32, y: Int32)`. Dalam blok lambda,
-# dilakukan perkalian `(x * y + 1)`. setelah menjalankan
+# dilakukan perkalian `{x * y + 1}`. setelah menjalankan
 # lambda melalui `.call(6, 2)`, adalah 13. Angka 13 tersebut
 # dicetak ke konsol menggunakan fungsi `puts`.
