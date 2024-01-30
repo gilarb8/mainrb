91365ab8ff3c27863bc550c3137deb4a0ba139e9
gilarc <gilarc@tuta.io>
2024-01-30 18:26:59 +0700

    item rev

--- simple_lambda.rb
+++ simple_lambda.rb
@@ -3,3 +3,5 @@
 # See LICENSE file for details.
 
 puts -> (x, y) {x * y + 1}.call(6, 2)
+
+# Kode diatas menggunakan lambda atau Proc dalam bahasa Ruby. Lambda tersebut dibuat dengan sintaks -> (x, y) dan memiliki blok kode {x * y + 1}. Melalui pemanggilan .call(6, 2), lambda tersebut dieksekusi dengan memberikan nilai 6 untuk x dan 2 untuk y. Hasilnya, yang adalah (6 * 2) + 1 = 13, ditampilkan ke konsol menggunakan fungsi puts.
