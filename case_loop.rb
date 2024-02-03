fee79d237c1d36a5e943a05439c3defe80e4118a
gilarc <gilarc@tuta.io>
2024-02-03 20:45:14 +0700

    code rev

--- case_loop.rb
+++ case_loop.rb
@@ -2,9 +2,6 @@
 # Copyright (c) 2024 Gillar Ajie Prasatya
 # See LICENSE file for details.
 
-sections = %W[web tools science systems
-		embedded drivers mobile]
-
 sections.each do |section|
   puts "Hello, #{section} developers!"
 end
