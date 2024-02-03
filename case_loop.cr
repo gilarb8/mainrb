59a40eb598787f9177ccaeb09a67f9c48d49437b
gilarc <gilarc@tuta.io>
2024-02-03 20:57:14 +0700

    add item

---
+++ case_loop.cr
@@ -0,0 +1,18 @@
+# MIT License
+# Copyright (c) 2024 Gillar Ajie Prasatya
+# See LICENSE file for details.
+
+arr : Array(String) = %w[ bandung jakarta surabaya ]
+
+arr.each do |sebut|
+  case sebut
+  when "bandung"
+    puts "kota lalab eater #{sebut}"
+  when "jakarta"
+    puts "kota ondel-ondel #{sebut}"
+  when "surabaya"
+    puts "kota kue lapis harga sejuta #{sebut}"
+  else
+    puts "kota tidak teridentifikasi: #{sebut}"
+  end
+end
