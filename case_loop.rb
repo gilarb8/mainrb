12227ec366373c11fb1301be41c2970b9f47e6bd
gilarc <gilarc@leim.luminouseclipseinmotion.com>
2024-02-03 20:39:41 +0700

    code rev

--- case_loop.rb
+++ case_loop.rb
@@ -1,3 +1,14 @@
+# MIT License
+# Copyright (c) 2024 Gillar Ajie Prasatya
+# See LICENSE file for details.
+
+sections = %W[web tools science systems
+		embedded drivers mobile]
+
+sections.each do |section|
+  puts "Hello, #{section} developers!"
+end
+
 arr = %w[bandung jakarta surabaya]
 
 arr.each do |sebut|
