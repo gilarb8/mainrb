dd7e36982fbc0030e45fa1630b932c3c4716f925
gilarc <gilarc@tuta.io>
2024-02-04 14:07:30 +0700

    code rev

--- simple_lambda_teston_arrloop.cr
+++ simple_lambda_teston_arrloop.cr
@@ -5,7 +5,8 @@
 number : Array(Int32) = [0,1,2,3,4,5,7,8,9]
 random : Array(Int32) = [0,20,321,512,7845,88]
 
-[-> { number.each { |pilah_number| puts "#{pilah_number}" } }, -> { random.each { |pilah_random| puts "#{pilah_random}" } }].each { |lambda_expr| lambda_expr.call }
+[-> { number.each { |pilah_number| puts "#{pilah_number}" } },
+ -> { random.each { |pilah_random| puts "#{pilah_random}" } }].each { |lambda_expr| lambda_expr.call }
 
 # Menggunakan array yang berisi dua lambda expression.
 # Lambda pertama mencetak elemen-elemen dari array "number".
