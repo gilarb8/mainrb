420ac1bfa6ed2b25d49491d6110221cc5c56a37d
gilarc <gilarc@tuta.io>
2024-02-04 13:03:26 +0700

    code rev

--- simple_lambda_teston_arrloop.rb
+++ simple_lambda_teston_arrloop.rb
@@ -3,5 +3,8 @@
 # See LICENSE file for details.
 
 number = [0,1,2,3,4,5,7,8,9]
+random = [0,20,321,512,7845,88] 
 
-puts -> (pilah){number.each do |pilah| puts "#{pilah}" end}.call(number)
+puts -> (pilah_number){number.each do |pilah_number| puts "#{pilah_number}" end}.call(number)
+puts "-----"
+puts -> (pilah_random){random.each do |pilah_random| puts "#{pilah_random}" end}.call(random)
