eaaf31eb3dfc15334b6921b41d9378edc223a2f3
gilarc <gilarc@tuta.io>
2024-02-04 12:51:53 +0700

    add item

---
+++ simple_lambda_teston_arrloop.rb
@@ -0,0 +1,7 @@
+# MIT License
+# Copyright (c) 2024 Gillar Ajie Prasatya
+# See LICENSE file for details.
+
+number = [0,1,2,3,4,5,7,8,9]
+
+puts -> (pilah){number.each do |pilah| puts "#{pilah}" end}.call(number)
