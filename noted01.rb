74fe7eacc9884e04697a1876d326c09b2dd217a5
gilarc <gilarc@tuta.io>
2024-02-04 14:08:59 +0700

    code rev

--- noted01.rb
+++ noted01.rb
@@ -1,3 +1,3 @@
 # penggunaan {} lebih umum dalam ekspresi singkat atau satu baris,
 # sementara do..end lebih umum digunakan untuk blok kode yang lebih
-# panjang atau multi-baris.
+# panjang atau multi-line.
